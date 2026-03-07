cd180784cd06cf67512bd36c04400208c1462b00
ato-z <523333404@qq.com>
2026-03-07 14:34:30 +0800

    fix: 业务解构

--- src/utils/LocalClientRTC.ts
+++ src/utils/LocalClientRTC.ts
@@ -1,58 +1,101 @@
 import DingRTC from 'dingrtc'
-import type { DingRTCClient } from 'dingrtc'
+import type { CameraVideoTrack, MicrophoneAudioTrack, RemoteTrack } from 'dingrtc'
 import { APPID } from '@/constant'
 import { getAliMeetingToken } from '@/api/meeting'
 
+type RemoteUser = { id: string }
 const instanceMap = new Map<string, LocalClientRTC>()
 
 export class LocalClientRTC {
+  /**
+   * 返回客户端实例
+   * @param uid
+   * @param roomId
+   */
   static getInstance(uid: string, roomId: string) {
     if (instanceMap.has(uid)) return instanceMap.get(uid)!
-
-    console.log('重启多次?')
     const instance = new LocalClientRTC(uid, roomId)
     instanceMap.set(uid, instance)
     return instance
   }
-  static instance: LocalClientRTC
 
-  protected start() {
+  /**
+   * 从后台获取阿里云token信息并加入客户端
+   */
+  protected async touchJoinByZerg() {
+    const certificate = await getAliMeetingToken(this.uid, this.roomId)
+
+    // 加入阿里会议
+    await this.client.join({
+      appId: APPID,
+      token: certificate.token,
+      uid: certificate.user_id,
+      channel: certificate.channel_id,
+      userName: this.uid,
+    })
+
+    return certificate
+  }
+
+  // start 远程轨道加入处理
+  /**
+   * 处理远程视频
+   */
+  private handleWithVideoTrack(userId: string, track: RemoteTrack) {
+    const playFn = Reflect.get(track, 'play').bind(track)
+    if (this.onRemoteUserJoin) {
+      this.onRemoteUserJoin({ id: userId }, playFn)
+    }
+  }
+
+  /**
+   * 处理远程音频
+   */
+  protected handleWithAudioTrack(track: RemoteTrack) {
+    if ('play' in track && typeof track.play === 'function') {
+      track.play()
+      this.handleWithAudioTrack = (track: RemoteTrack) => console.log('远程音频仅需监听1次', track)
+    } else {
+      console.error('播放远程音频失败')
+    }
+  }
+
+  /**  */
+  protected async setupClientEventListeners() {
+    // 等待加入完毕
+    await this.joinPromise
     const { client } = this
 
+    // 处理远程音视频轨道
     client.on('user-published', (user, mediaType, auxiliary) => {
-      console.log('远程用户加入', client.remoteUsers)
       if (mediaType === 'video') {
-        console.log('远程uid', user.userId)
-        client.subscribe(user.userId, mediaType, auxiliary).then((track) => {
-          const playFn = Reflect.get(track, 'play').bind(track)
-          if (this.onRemoteUserJoin) {
-            this.onRemoteUserJoin({ id: user.userId }, playFn)
-          }
-        })
-      } else if (!this.mcuSubscribed) {
-        this.mcuSubscribed = true
-        client.subscribe('mcu', 'audio').then((track) => {
-          const playFn = Reflect.get(track, 'play')
-          if (typeof playFn === 'function') {
-            // playFn.apply(track, [])
-          }
-        })
+        return this.client
+          .subscribe(user.userId, 'video', auxiliary)
+          .then((track) => this.handleWithVideoTrack(user.userId, track))
+      }
+      if (mediaType === 'audio') {
+        return this.client.subscribe('mcu', 'audio').then((track) => this.handleWithAudioTrack(track))
       }
     })
 
+    // 处理远程用户退出
     client.on('user-unpublished', (user) => {
       if (this.onRemoveUserLeave) {
         this.onRemoveUserLeave({ id: user.userId })
       }
     })
   }
+  // 远程轨道加入处理 end
 
-  protected async touchJoinRemove() {
-    const { remoteUsers } = this.client
+  // start 远程订阅
+  /** */
+  protected async touchSubParams() {
+    // 订阅参数
     const subParams: Array<{ uid: string; mediaType: 'audio' | 'video'; auxiliary: boolean }> = [
-      { uid: 'mcu', mediaType: 'audio', auxiliary: false },
+      { uid: 'mcu', mediaType: 'audio', auxiliary: false }, // 远程音频
     ]
-    remoteUsers.forEach((user) => {
+    // 远程视频轨道
+    this.client.remoteUsers.forEach((user) => {
       if (user.hasAuxiliary) {
         subParams.push({ uid: user.userId, mediaType: 'video', auxiliary: true })
       }
@@ -61,76 +104,74 @@ export class LocalClientRTC {
       }
     })
 
+    return subParams
+  }
+
+  /** 订阅远程视频视频流 */
+  protected async batchSubscribeRemoteMedia() {
+    // 等待加入完毕
+    await this.joinPromise
+    const subParams = await this.touchSubParams()
     const batchSubscribeResult = await this.client.batchSubscribe(subParams)
+
     for (const { error, track, uid, auxiliary } of batchSubscribeResult) {
+      if (!track) continue
       if (error) {
         console.log(`[订阅${uid} ${auxiliary ? 'screenShare' : 'camera'} 失败]` + error.message, 'danger')
         continue
       }
-      if (!track) return void 0
-      if (track.trackMediaType === 'audio' && this.mcuSubscribed) {
-        this.mcuSubscribed = true
-        const playFn = Reflect.get(track, 'play')
-        if (typeof playFn === 'function') {
-          playFn.apply(track, [])
-        }
-      } else {
-        const playFn = Reflect.get(track, 'play').bind(track)
-        if (this.onRemoteUserJoin) {
-          this.onRemoteUserJoin({ id: uid }, playFn)
-        }
-      }
+
+      if (track.trackMediaType === 'audio') this.handleWithAudioTrack(track)
+      else if (track.trackMediaType === 'video') this.handleWithVideoTrack(uid, track)
     }
   }
+  // 远程订阅加入处理 end
 
+  /** */
   leave() {
     console.log('用户退出房间')
-    /* if (this.client) {
-      try {
-        this.client.leave()
-      } catch {
-        console.log('退出房间失败')
-      }
-    }
-
-    instanceMap.delete(this.uid)*/
   }
 
+  /** 创建视频流 */
+  async createCameraVideoTrack() {
+    if (this.localVideoTrack) return this.localVideoTrack
+    this.localVideoTrack = await DingRTC.createCameraVideoTrack({
+      frameRate: 15,
+      dimension: 'VD_1280x720',
+    })
+
+    return this.localVideoTrack
+  }
+
+  /** 创建音频流 */
+  async createMicrophoneAudioTrack() {
+    if (this.localAudioTrack) return this.localAudioTrack
+    this.localAudioTrack = await DingRTC.createMicrophoneAudioTrack()
+    return this.localAudioTrack
+  }
+
+  /** */
   private constructor(
     public readonly uid: string,
     public readonly roomId: string
   ) {
-    const client = DingRTC.createClient()
-    this.client = client
-
-    this.start()
-    this.joinPromise = this.withByOnline().then(async (res) => {
-      await this.touchJoinRemove()
-      return res
-    })
+    this.client = DingRTC.createClient()
+    this.joinPromise = this.touchJoinByZerg()
+    this.setupClientEventListeners()
+    this.batchSubscribeRemoteMedia()
   }
 
-  private async withByOnline() {
-    const { uid, roomId } = this
-    const { token, channel_id, user_id } = await getAliMeetingToken(uid, roomId)
-
-    console.log(token, channel_id, user_id)
-    return this.client.join({
-      appId: APPID,
-      token: token,
-      uid: user_id,
-      channel: channel_id.toString(),
-      userName: `user: ${uid}`,
-    })
-  }
-
+  // 阿里云本地客户端
   public readonly client
-  public readonly joinPromise
-  public mcuSubscribed = false
-  public onRemoteUserJoin?: (
-    user: DingRTCClient['remoteUsers'][number] | { id: string },
-    play: (id: string) => void
-  ) => void
+  // 是否加入房间完毕
+  public readonly joinPromise: ReturnType<typeof getAliMeetingToken>
+  // 远程用户加入
+  public onRemoteUserJoin?: (user: RemoteUser, play: (id: string) => void) => void
+  // 远程用户退出
+  public onRemoveUserLeave?: (user: RemoteUser) => void
 
-  public onRemoveUserLeave?: (user: DingRTCClient['remoteUsers'][number] | { id: string }) => void
+  // 本地视频流
+  public localVideoTrack?: CameraVideoTrack
+  // 本地音频流
+  public localAudioTrack?: MicrophoneAudioTrack
 }
